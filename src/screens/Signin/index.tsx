259a11d327e447b5435d2a672ad641621273bd0e
Daniel Borges <dlopesborges@gmail.com>
2021-06-29 13:59:02 -0300

    Aula 2

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -2,22 +2,24 @@ import React from 'react';
 import { 
   View, 
   Text, 
-  Image,
-  StatusBar
+  Image
 } from 'react-native';
 
+import { styles } from './styles';
 import { ButtonIcon } from '../../components/ButtonIcon';
 import IllustrationImg from '../../assets/illustration.png';
-import { styles } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 export function SignIn() {
+  
+  const navigation = useNavigation();
+
+  function handleSignIn(){
+    navigation.navigate('Home');
+  }
+
   return(
     <View style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
       <Image 
        source={IllustrationImg} 
        style={styles.image}
@@ -27,9 +29,9 @@ export function SignIn() {
       <View style={styles.content}>
 
         <Text style={styles.title}>
-          Organize{`\n`} 
-          suas jogatinas{`\n`} 
-          facilmente
+          Conecte-se{`\n`} 
+          e organize{`\n`} 
+          suas jogatinas
         </Text>
 
         <Text style={styles.subtitle}>
@@ -37,7 +39,10 @@ export function SignIn() {
           favoritos com seus amigos 
         </Text>
 
-        <ButtonIcon title="Entrar com Discord" activeOpacity={0.7}/>
+        <ButtonIcon 
+         title="Entrar com Discord" 
+         onPress={handleSignIn}
+        />
       </View>
     </View>
   );
